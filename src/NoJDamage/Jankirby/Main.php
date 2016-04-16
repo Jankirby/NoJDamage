7e4c62cd437603de415ea5f064dc44132093c45b
Jan Kirby <coolguy412@mailinator.com>
2016-04-16 18:09:15 +0800

    Update Main.php

--- src/NoJDamage/Jankirby/Main.php
+++ src/NoJDamage/Jankirby/Main.php
@@ -10,13 +10,13 @@ class Main extends PluginBase implements Listener{
   public function onEnable(){
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
-     public function onDamage(EntityDamageEvent $event){
-        if($event->getEntity() instanceof Player){
-            if($event->getCause() === EntityDamageEvent::CAUSE_FALL){
-                 $event->setCancelled(true);
-                 $event->setDamage(0);
-                 $event->getPlayer()->sendMessage("You fell very high, but the damage was cured from your feet.");
-            }
-        }
-     }
+         public function onDamage(EntityDamageEvent $event){
+         if($event->getEntity() instanceof Player){
+         if($event->getCause() == EntityDamageEvent::CAUSE_FALL){
+         $event->setCancelled();
+         $event->sePlayer()->sendMessage("You fell very high, but the damage was cured from your feet.");
+         }
+         }
+         }
 }
+
