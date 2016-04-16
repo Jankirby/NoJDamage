267b674d84e8c825a791db32d7a0dd5900f56c72
Jan Kirby <coolguy412@mailinator.com>
2016-04-16 16:40:44 +0800

    Update Main.php

--- src/NoJDamage/Jankirby/Main.php
+++ src/NoJDamage/Jankirby/Main.php
@@ -6,14 +6,14 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageEvent;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener{
   public function onEnable(){
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
-     public function onDamage(EntityDamageEvent $event){  
-        if ($event->getEntity() instanceof Player) {
-            if ($event->getCause() === EntityDamageEvent::CAUSE_FALL) {
-                 $event->setCancelled();
+     public function onDamage(EntityDamageEvent $event){
+        if($event->getEntity() instanceof Player){
+            if($event->getCause() === EntityDamageEvent::CAUSE_FALL){
+                 $event->setCancelled(true);
                  $event->setDamage(0);
                  $event->getPlayer()->sendMessage("You fell very high, but the damage was cured from your feet.");
             }
