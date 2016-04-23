8a4413e4f4cbeb2a04ad2c56b834e82bb0bb3c97
Jan Kirby <coolguy412@mailinator.com>
2016-04-23 21:08:23 +0800

    Update Main.php

--- src/NoJDamage/Jankirby/Main.php
+++ src/NoJDamage/Jankirby/Main.php
@@ -6,17 +6,20 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageEvent;
 
-class Main extends PluginBase implements Listener {
-  public function onEnable(){
+class Main extends PluginBase implements Listener{
+    
+    public function onEnable(){
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->info("[CyroPE NoJDamage] enabled!");
   }
-     public function onDamage(EntityDamageEvent $event){  
-        if ($event->getEntity() instanceof Player) {
-            if ($event->getCause() === EntityDamageEvent::CAUSE_FALL) {
-                 $event->setCancelled();
-                 $event->setDamage(0);
-                 $event->getPlayer()->sendMessage("You fell very high, but the damage was cured from your feet.");
-            }
-        }
-     }
+public function onFall(EntityDamageEvent $event){
+		$entity = $event->getEntity();
+		$cause = $event->getCause();
+		if($this->hasFall($entity)){
+			if($cause == EntityDamageEvent::CAUSE_FALL){
+				$event->setCancelled(true);
+                                $event->getPlayer()->sendTip("You fell very high, but the damage was cured from your feet.");
+                        }
+                }
+}
 }
