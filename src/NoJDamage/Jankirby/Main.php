a79434a6c934856048984cb652fb58996d19132a
Jan Kirby <coolguy412@mailinator.com>
2016-04-24 10:39:04 +0800

    Update Main.php

--- src/NoJDamage/Jankirby/Main.php
+++ src/NoJDamage/Jankirby/Main.php
@@ -16,8 +16,11 @@ public function onFall(EntityDamageEvent $event){
 		$entity = $event->getEntity();
 		$cause = $event->getCause();
 		if($this->hasFall($entity)){
+                    $event->setCancelled(true);
+                      $player->setHealth(20);
 			if($cause == EntityDamageEvent::CAUSE_FALL){
 				$event->setCancelled(true);
+                                 $player->setHealth(20);
                                 $event->getPlayer()->sendTip("You fell very high, but the damage was cured from your feet.");
                         }
                 }
