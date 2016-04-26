3d7a543e3bd417ebe3a7858db2c4834b07dd3098
Jan Kirby <coolguy412@mailinator.com>
2016-04-26 16:44:39 +0800

    Update!

--- src/NoJDamage/Jankirby/Main.php
+++ src/NoJDamage/Jankirby/Main.php
@@ -5,23 +5,26 @@ namespace NoJDamage\Jankirby;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageEvent;
-
+/*
+ * Copyright (c) 2016 Jankirby
+ */
 class Main extends PluginBase implements Listener {
-  public function onEnable(){
+
+    private $cause;
+
+    public function onEnable(){
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
      public function onDamage(EntityDamageEvent $event){  
-         $p = $event->getEntity();
-         $x = false;
+         $entity = $event->getEntity();
+        /* @var $cause type */
+         $cause = $event->getCause();
         if ($event->getEntity() instanceof Player) {
             if ($event->getCause() === EntityDamageEvent::CAUSE_FALL) {
-                $event->setCancelled();
-      if(!$this->canGetHurt($p)) {
-        $event->setCancelled();
-                 $event->getPlayer()->sendMessage("You fell very high, but the damage was cured from your feet.");
+                $sendMessage = $event->getPlayer()->sendMessage(TextFormat::RED ."You fell very high, but the damage was cured from your feet.");
+                $setCancelled = $event->setCancelled();
+                return $this->cause; 
             }
         }
      }
 }
-}
-
